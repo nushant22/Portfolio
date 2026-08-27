878838fed356be971bd391363c6039b259c6527a
nushant22 <nushantghimire22@gmail.com>
2026-08-27 22:13:13 +0545

    fix: suppress hydration warning caused by browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
-      <body className="font-sans">
+      <body className="font-sans" suppressHydrationWarning>
         {children}
       </body>
     </html>
